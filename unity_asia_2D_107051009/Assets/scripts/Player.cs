29b4d4514840e7e270465a3dca08a3e190ed2ed0
a0975660266 <71679957+a0975660266@users.noreply.github.com>
2020-11-10 23:20:54 +0800

    期中完成

---
+++ unity_asia_2D_107051009/Assets/scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public GameObject final;
+
+    public Text textcount;
+
+    public int count;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name == "傳送門")
+        {
+           final.SetActive(true);
+        }
+
+        if(collision.tag == "酒瓶")
+        {
+            Destroy(collision.gameObject);
+
+            count++;
+
+            textcount.text = "酒瓶數量:" + count;
+        }
+    }
+
+}
